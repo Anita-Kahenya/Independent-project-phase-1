b6bafe30b13b8bafbe622b04cf59e3e3aab5ea18
+ <+>
2022-12-16 16:02:37 +0300

    reference to the html form and adding event listener

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",() =>{
+    fetch("http://localhost:3000/Guests")
+    .then(response => response.json())
+    .then(data =>(data))
+})
+
+let form = document.getElementById("styleForm")
+form = document.addEventListener("submit",addNewGuestDetails)
+
+function addNewGuestDetails(event){
+  event.preventDefault()
+  let guestDetails = {
+    firstname: event.target.firstname.value,
+    lastname: event.target.lastname.value,
+    email: event.target.email.value,
+    seatno: event.target.seatno.value,
+    typeofaccess: event.target.typeofaccess.value
+  }
+}
+
+
